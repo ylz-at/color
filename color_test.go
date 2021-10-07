2ca0a9a5c47363a5ab496abec00b37b4a324780b
Jinesi Yelizati <i63888888@163.com>
2021-10-07 21:59:29 +0800

    test(): add test for rgb2xyz
    Signed-off-by: Jinesi Yelizati <i63888888@163.com>

--- color_test.go
+++ color_test.go
@@ -42,3 +42,44 @@ func Test_xyz2rgb(t *testing.T) {
 		})
 	}
 }
+
+func Test_rgb2xyz(t *testing.T) {
+	type args struct {
+		r float64
+		g float64
+		b float64
+	}
+	tests := []struct {
+		name  string
+		args  args
+		wantX float64
+		wantY float64
+		wantZ float64
+	}{
+		{
+			"normal",
+			args{
+				r: 0.4171,
+				g: 0.7434,
+				b: 0.2152,
+			},
+			0.25,
+			0.40,
+			0.10,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotX, gotY, gotZ := rgb2xyz(tt.args.r, tt.args.g, tt.args.b)
+			if gotX != tt.wantX {
+				t.Errorf("rgb2xyz() gotX = %v, want %v", gotX, tt.wantX)
+			}
+			if gotY != tt.wantY {
+				t.Errorf("rgb2xyz() gotY = %v, want %v", gotY, tt.wantY)
+			}
+			if gotZ != tt.wantZ {
+				t.Errorf("rgb2xyz() gotZ = %v, want %v", gotZ, tt.wantZ)
+			}
+		})
+	}
+}
